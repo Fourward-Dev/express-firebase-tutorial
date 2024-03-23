fdf7abe91e6df4d1ed87fd1da32e4727e3919984
Aymen-Dev-tech <bouaymen488@gmail.com>
2024-03-23 12:40:54 +0100

    Update firebase config: load firebase credentials through dotenv

--- firebas.config.js
+++ firebas.config.js
@@ -1,29 +1,22 @@
-import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
+/* eslint-disable no-undef */
+import { initializeApp, cert } from "firebase-admin/app";
 
-import {
-  getFirestore,
-  Timestamp,
-  FieldValue,
-  Filter,
-} from "firebase-admin/firestore";
+import { getFirestore } from "firebase-admin/firestore";
 
-import serviceAccount from "./secrets.json" assert { type: "json" };
-
-const serviceAccountStr = {
-  type: process.env.type,
-  project_id: process.env.project_id,
-  private_key_id: process.env.private_key_id,
-  private_key: process.env.private_key,
-  client_email: process.env.client_email,
-  client_id: process.env.client_id,
-  auth_uri: process.env.auth_uri,
-  token_uri: process.env.token_uri,
-  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-  client_x509_cert_url: process.env.client_x509_cert_url,
-  universe_domain: process.env.universe_domain,
-};
 initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key?.replace(/\\n/g, "\n"),
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+    universe_domain: process.env.universe_domain,
+  }),
 });
 
 const db = getFirestore();
